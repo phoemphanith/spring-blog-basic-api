24ed9f2fd0fc9207ed54cd053e7e9f95b6b9a8f3
phoemphanith <phanith1826@gmail.com>
2023-02-20 14:32:04 +0700

    Start Project

---
+++ src/main/kotlin/com/phoemphanith/blogserviceapibasic/BlogServiceApiBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.phoemphanith.blogserviceapibasic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogServiceApiBasicApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogServiceApiBasicApplication>(*args)
+}
---
+++ src/test/kotlin/com/phoemphanith/blogserviceapibasic/BlogServiceApiBasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.phoemphanith.blogserviceapibasic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogServiceApiBasicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
